8d563de3c7dbca551a963c8769d8d59606644527
Ijlal Ahmad <ijlalahmad845@gmail.com>
2025-09-02 23:09:37 +0530

    🗑️ chore: remove obsolete ESLint and TypeScript configuration files to streamline project setup and maintenance.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,7 @@
+import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import json from '@eslint/json';
-import markdown from '@eslint/markdown';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
-import _import from 'eslint-plugin-import';
-import prettier from 'eslint-plugin-prettier';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import prettier from 'eslint-config-prettier';
 
 export default [
   {
@@ -35,35 +19,18 @@ export default [
       '**/*.md',
     ],
   },
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
-  ),
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     languageOptions: {
       globals: globals.node,
-      parser: tsParser,
-      ecmaVersion: 2023,
+      ecmaVersion: 'latest',
       sourceType: 'module',
-      parserOptions: {
-        project: 'tsconfig.eslint.json',
-        tsconfigRootDir: __dirname,
-      },
-    },
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-      import: _import,
-      prettier: prettier,
-    },
-    rules: {
-      'prettier/prettier': 'error',
     },
   },
-  ...tseslint.configs.recommended,
-  { files: ['**/*.json'], plugins: { json }, language: 'json/json' },
-  { files: ['**/*.jsonc'], plugins: { json }, language: 'json/jsonc' },
-  { files: ['**/*.json5'], plugins: { json }, language: 'json/json5' },
-  { files: ['**/*.md'], plugins: { markdown }, language: 'markdown/gfm' },
+
+  prettier,
 ];
